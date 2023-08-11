49e98043b4c7b3258a8bd2fc9d391528361c371a
gurmanbenipal <134737006+gurmanbenipal@users.noreply.github.com>
2023-08-11 03:56:09 -0700

    done part 4

--- src/NewSkillForm.jsx
+++ src/NewSkillForm.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function NewSkillForm() {
+const defaultSkillValue = {
+    name: '',
+    level: 3
+}
+export default function NewSkillForm({addSkill}) {
+    const [formData, setFormData] = useState(defaultSkillValue);
+
+    function handleChange(evt){
+        setFormData({...formData, [evt.target.name]:
+        evt.target.value})
+    }
+    function handleAddSkill(evt){
+        evt.preventDefault()
+        addSkill(formData);
+        setFormData(defaultSkillValue)
+    }
     return (
         <>
-            <form action="" className="NewSkillForm">
+            <form className="NewSkillForm" onSubmit={handleAddSkill}>
                 <label htmlFor="">Skill</label>
-                <input className='newskillformbtn'></input>
+                <input className='newskillformbtn' name='name' onChange={handleChange} value={formData.name}></input>
                 <label>Level</label>
-                <select className='newskillformselect'>
-                    <option>1</option>
-                    <option>2</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
+                <select className='newskillformselect'name="emotion" onChange={handleChange} value={formData.level}>
+                    <option value={1}>1</option>
+                    <option value={2}>2</option>
+                    <option value={3}>3</option>
+                    <option value={4}>4</option>
+                    <option value={5}>5</option>
                 </select>
                 <button>ADD SKILL</button>
             </form>
